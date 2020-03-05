cbfa84fd63e9b6cf491898892da04178629078b0
medhinioak <medhinioak@gmail.com>
2020-03-06 05:11:24 +0530

    fixed tests

--- src/containers/CartPage/index.test.jsx
+++ src/containers/CartPage/index.test.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import CartPage from './index';
 
 
 describe('the cart page container', () => {
+  const history = createMemoryHistory();
   it('should render correctly', () => {
-    const { asFragment } = render(<CartPage />);
+    const { asFragment } = render(<Router history={history}><CartPage /></Router>);
 
     expect(asFragment()).toMatchSnapshot();
   });
